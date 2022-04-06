ce4fd68fef9c8205a20d29b68726222097b184d1
emmanil <emma.nilsson.567@gmail.com>
2022-04-06 11:13:52 +0200

    first commit 6/4)

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+
+
+public class Main {
+
+        public static void main(String[] args) {
+                System.out.println("Story:");
+
+
+                a("Once upon a time there was a boy.");
+                e("and a girl. They loved git.");
+                e("Or at least they tried to make sens of git.");
+                e("it took many attempts");
+                e("and much coffee!");
+                e("soooo much coffee!");
+
+                e("\n Today 6/4 Emma open git Bash and then she wrote\n" +
+                        "git branch -a //So that she could see all the branches. ");
+                e("She then wrote \ngit status //to see what branch she was in. ");
+                e("She was in the correct branch so she wrote:\n" +
+                        "git pull //to get the latest\n");
+
+                e("She then wrote some sentences and the she wrote\n" +
+                        "git add . //to add all\n" +
+                        "git commit -m \"nytt 6/4\"//to commit all.");
+
+
+
+
+            }
+
+            public static void a(String words) {
+                System.out.println(words);
+            }
+
+
+            public static void e(String words) {
+                System.out.println(words);
+            }
+
+
+
+}
